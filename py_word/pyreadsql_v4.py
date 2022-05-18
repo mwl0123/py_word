1197d291e2f98bf6737fb1c9d9a812d683e3c0b8
mwl0123 <mwl0123@outlook.com>
2022-05-18 11:07:35 +0000

    20220518

--- py_word/pyreadsql_v4.py
+++ py_word/pyreadsql_v4.py
@@ -3,15 +3,17 @@ from docx.shared import Inches
 from docx.oxml import OxmlElement, ns
 import re
 
-fd = open("/Users/wailunmok/MyProject/Python/py_word/sql/sp_sample.sql", "r")
+fd = open("/workspace/py_word/py_word/sql/sp_sample.sql", encoding="ISO-8859-1")
 sqlFile = fd.read()
+#print(sqlFile)
+print('疑癮')
 sq_result = re.findall('insert(.*)values|from(.*)where|update(.*)set', sqlFile)
 
 wh_result = re.findall(r'(\w+) = (\w+)', sqlFile)
-print('start')
-print(sq_result)
-print(wh_result)
-print('end')
+#print('start')
+#print(sq_result)
+#print(wh_result)
+#print('end')
 
 #iq_result = re.findall('insert(.*)values', sqlFile)
 #print(iq_result)
@@ -21,7 +23,5 @@ print('end')
 
 
 fd.close()
-
 s = 'asdf=5;iwantthis123jasd'
 result = re.search('asdf=5;(.*)123jasd', s)
-print(result.group(1))
